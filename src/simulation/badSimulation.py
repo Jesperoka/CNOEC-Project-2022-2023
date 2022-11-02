fe2124ad7bd7182cae39586d3b5c5123be24caac
Jesper <jesperoka@gmail.com>
2022-11-02 22:11:02 +0100

    wrapped some matplotlib plotting functionality in helper functions to standardize plots

---
+++ src/simulation/badSimulation.py
@@ -0,0 +1,51 @@
+import matlab.engine
+from math import sin
+from math import pi
+from random import uniform
+from ..visualization import plotHelpers
+
+# WARNING THIS FILE IS CURRENTLY UGLY AND WILL BE CHANGED
+
+if __name__ == "__main__":
+    # Setting up MATLAB Engine
+    m = matlab.engine.start_matlab()
+    m.cd("src/innerLoop/solarPanels")
+
+    # Loading constants
+    pvParams = m.constants()
+
+    # Simulation setup
+    phaseLag = pi/9
+    T_p_0 = 273.15 + 20*sin(-phaseLag) + 10*uniform(0.5, 1.5) # Kelvin
+    t0 = 0
+    tf = 1000
+    tspan = [t0, tf]
+    epsilon = 0.18
+    maxSolarIrradiance = 1000 # W/m^2
+
+    # Generating some basic solar irradiance and ambient temperature data. Just sine curves.
+    G   = [uniform(0.999, 1.001)*maxSolarIrradiance*sin((2*pi)*(t/tf)) if sin((2*pi)*(t/tf)) >= 0 else 0.0 for t in range(0, tf + 1)]
+    T_a = [273.15 + 20*sin(2*pi*(t/tf) - phaseLag) + 10*uniform(0.999, 1.001) for t in range(0, tf + 1)]
+
+    # Putting Python variables in MATLAB workspace.
+    m.workspace['T_p_0'] = float(T_p_0)
+    m.workspace['epsilon'] = float(epsilon)
+    m.workspace['G'] = m.cell2mat(G)
+    m.workspace['T_a'] = m.cell2mat(T_a)
+    m.workspace['pvParams'] = pvParams
+    m.workspace['t0'] = float(t0)
+    m.workspace['tf'] = float(tf)
+   
+    # Because we need to pass a function type to ode45, we need to use eval().
+    t, T_p = m.eval('ode15s( @(t, T_p) pvTemperatureDynamics(t, T_p, T_a, G, epsilon, pvParams), [t0 tf], T_p_0)', nargout=2)
+    m.exit()
+
+    # Basic plot
+    plotHelpers.standardizedPlot([[t, T_p], T_a, G], 
+        labels=["Panel Temperature (T_p)", "Ambient Temperature (T_a)", "Solar Irradiance (G)"],
+        xlabel="time",
+        ylabel="temperature (K)",
+        ylabel2="irradiance (W m^-2)",
+        title="Solar Panel Temperature Dynamics",
+        twin=True,
+        twinSep=2)
---
+++ src/visualization/plotHelpers.py
@@ -0,0 +1,54 @@
+# TODO: file description
+import matplotlib.pyplot as plt
+
+# TODO: comment
+colorProfiles = {"dark": 
+{"foregroundColor": "#BBBBBB", "backgroundColor": "#191919","graphColors": ["#6E975C", "#1E4150", "#DA7A41", "#D0C58E"]}}
+
+# TODO: comment
+def plotLabeledData(ax, labeledData, graphColors):
+    for i, (dataArray, label) in enumerate(labeledData):
+        if len(dataArray) == 2: # quick fix
+            ax.plot(*dataArray, label=label, color=graphColors[i])
+        else:
+            ax.plot(dataArray, label=label, color=graphColors[i])
+
+# TODO: comment        
+def getColors(colorProfile="dark"):
+    foregroundColor = colorProfiles[colorProfile]["foregroundColor"]
+    backgroundColor = colorProfiles[colorProfile]["backgroundColor"]
+    graphColors = colorProfiles[colorProfile]["graphColors"]
+    return foregroundColor, backgroundColor, graphColors
+
+# TODO: comment
+def stylizeFigure(fig, axes, title, xlabel, ylabels, foregroundColor, backgroundColor, labelFontSize):
+    for ax, ylabel in zip(axes, ylabels):
+        plt.setp(ax.spines.values(), color=foregroundColor)
+        ax.tick_params('both', colors=foregroundColor)
+        ax.set_ylabel(ylabel, color=foregroundColor, fontsize=labelFontSize)
+        ax.set_facecolor(backgroundColor)
+    axes[0].set_xlabel(xlabel, color=foregroundColor, fontsize=labelFontSize)
+    fig.suptitle(title, color=foregroundColor, fontsize=1.5*labelFontSize)
+    fig.set_facecolor(backgroundColor)
+    fig.set_edgecolor("none")
+    fig.legend(facecolor=backgroundColor, edgecolor=foregroundColor, labelcolor=foregroundColor)
+
+# TODO: comment
+def standardizedPlot(data, labels, title="Standardized Plot", xlabel="x-axis", ylabel="y-axis-1", ylabel2="y-axis-2", colorProfile="dark", twin=False, twinSep=None, figsize=(8, 6), labelFontSize=14):
+    fig, ax1 = plt.subplots(sharey = False, figsize=(8, 6))
+    foregroundColor, backgroundColor, graphColors = getColors(colorProfile=colorProfile)
+    if twin:
+        axes = [ax1, ax1.twinx()]
+        labeledData = zip(data[:twinSep], labels[:twinSep])
+        TwinLabeledData = zip(data[twinSep:], labels[twinSep:])
+        plotLabeledData(axes[0], labeledData, graphColors[:twinSep])
+        plotLabeledData(axes[1], TwinLabeledData, graphColors[twinSep:])
+        ylabels = [ylabel, ylabel2]
+    else:
+        axes = [ax1]
+        labeledData = zip(data, labels)
+        plotLabeledData(axes[0], labeledData, graphColors)
+        ylabels = [ylabel] 
+    stylizeFigure(fig, axes, title, xlabel, ylabels, foregroundColor, backgroundColor, labelFontSize)
+    plt.show()
+    return fig, axes
