07c84b61e370cb3f63645f11812c5b6a65c4e62b
Jesper Ekstrøm <jesperek@stud.ntnu.no>
2022-11-30 17:10:25 +0100

    implemented battery dynamics function

--- src/innerLoop/batteries/batteryDynamics.m
+++ src/innerLoop/batteries/batteryDynamics.m
@@ -1,1 +1,13 @@
 % placeholder file
+function C_b_dot = batteryDynamics(P_b, battParams)
+    V_oc = battParams.openCircuitVoltage
+    R_0 = battParams.batteryResistance
+    Q_nom = battParams.nominalCapacity
+    
+    I_b = batteryCurrent(P_b, V_oc, R_0)
+    C_b_dot = (1/Q_nom)*I_b
+end
+
+function I_b = batteryCurrent(P_b, V_oc, R_0)
+    I_b = V_oc/(2*R_0) - ((V_oc/(2*R_0))^2 - (P_b/R_0))^(1/2)
+end
