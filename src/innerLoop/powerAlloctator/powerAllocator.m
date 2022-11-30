7015ac62126d52a9592806350339a6389c2faf99
Jesper Ekstrøm <jesperek@stud.ntnu.no>
2022-11-30 17:12:35 +0100

    battery vs grid power splitter

---
+++ src/innerLoop/powerAlloctator/powerAllocator.m
@@ -0,0 +1,6 @@
+% Outputs the amount of power to draw from battery storage vs grid power, based on gamma and P_diff.
+function P_out_g, P_out_b = powerAllocator(P_d, P_in_h, gamma)
+    P_diff = P_d - P_in_h
+    P_out_g = P_diff*gamma
+    P_out_b = P_diff*(1 - gamma)
+end
