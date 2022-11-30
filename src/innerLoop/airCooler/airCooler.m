742201bb1e7aad5427e33749dedf0a98c5685abb
Jesper <jesperoka@gmail.com>
2022-12-01 00:52:00 +0100

    basic electric fan air cooler

--- src/innerLoop/airCooler/airCooler.m
+++ src/innerLoop/airCooler/airCooler.m
@@ -1,8 +1,21 @@
 % placeholder file
-function V_air = airCooler1(I_in, inputs)
+function V_air = airCooler(I_in)
     % compute dynamics of air cooling system
     %   - compressed air -> pre-charge compressed air in tank
     %   - electric fan
     %   
-    V_air = Inputs*I_in
+
+    areaOfPvPanel   = 0.7749; % m^2
+    numberOfPanels  = 1;
+
+    % Placeholder industrial electric fan
+    % https://noctua.at/en/nf-f12-industrialppc-3000-pwm/specification
+    maxAirflow      = 186.7/(60*60); % m^3 s^-1
+    maxCurrent      = 0.3;           % A
+    area            = pi*0.118^2;         % m^2
+
+    numberOfFans    = ceil(numberOfPanels*(areaOfPvPanel)/area);
+    currentPerFan   = I_in/numberOfFans;
+    
+    V_air           = (maxAirflow/maxCurrent)*currentPerFan;
 end
