545c990fef632eb0535cdd45f4a3b7556d1c76f4
Jesper Ekstrøm <jesperek@stud.ntnu.no>
2022-11-30 17:10:54 +0100

    placeholder airCooler dynamics file

---
+++ src/innerLoop/airCooler/airCooler.m
@@ -0,0 +1,8 @@
+% placeholder file
+function V_air = airCooler1(I_in, inputs)
+    % compute dynamics of air cooling system
+    %   - compressed air -> pre-charge compressed air in tank
+    %   - electric fan
+    %   
+    V_air = Inputs*I_in
+end
