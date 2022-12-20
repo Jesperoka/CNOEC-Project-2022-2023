88b813aeaf767baa1c9d7094eee2eeddb4ee3966
Jesper Ekstrøm <jesperek@stud.ntnu.no>
2022-12-20 21:16:15 +0100

    renamed outputs to better reflect actual meaning

--- src/innerLoop/powerAlloctator/pvPowerSplitter.m
+++ src/innerLoop/powerAlloctator/pvPowerSplitter.m
@@ -1,6 +1,6 @@
 % Splits P_tot into 3 components based on values of alpha and beta, where sum(alpha, beta) in [0, 1]
-function [P_in_h, P_in_g, P_in_b] = pvPowerSplitter(P_p, alpha, beta)
-    P_in_h = P_p*(1 - alpha - beta);
-    P_in_g = P_p*alpha;
-    P_in_b = P_p*beta;
+function [P_p_in_h, P_p_in_g, P_p_in_b] = pvPowerSplitter(P_p, alpha, beta)
+    P_p_in_h = P_p*(1 - alpha - beta);
+    P_p_in_g = P_p*alpha;
+    P_p_in_b = P_p*beta;
 end
