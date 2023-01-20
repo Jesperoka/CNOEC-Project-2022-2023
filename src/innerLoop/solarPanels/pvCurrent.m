62669e956286bc24962ca172a0ddd51cf4761bd0
Jesper <jesperoka@gmail.com>
2023-01-20 17:38:25 +0100

    bufix: wrong parameter struct name

--- src/innerLoop/solarPanels/pvCurrent.m
+++ src/innerLoop/solarPanels/pvCurrent.m
@@ -1,15 +1,15 @@
 % TODO: description and improve readability (also complete parameter getting)
 function I_p = pvCurrent(I_p, T_p, G, V, pvPowerParams)
-    q   = pvPowerParams.electronCharge;
-    k   = pvPowerParams.boltzmannConstant;
-    Tr  = pvPowerParams.nominalTemperature; % nominal temperature
+    q   = pvCurrentParams.electronCharge;
+    k   = pvCurrentParams.boltzmannConstant;
+    Tr  = pvCurrentParams.nominalTemperature; % nominal temperature
     Eg0 = 1.1;
     Voc = 21.6;
     Isc = 6.11;
     Ki  = 0.002;
-    Ns  = 36;
-    Np  = 2;
-    n   = 1.6;
+    Ns  = 36;          % panels connected in series
+    Np  = 2;           % panels connected ii parallel
+    n   = 1.6;         % ideality factor of diode
     Rs  = 1e-4; % Realistically this not a constant if we want to implement MPPT
     Rsh = 1e3;
 
@@ -23,6 +23,7 @@ function I_p = pvCurrent(I_p, T_p, G, V, pvPowerParams)
     Irs     = Isc/(exp(q*Voc/(Ns*k*n*T_p))-1);
     I0      = Irs*((T_p/Tr)^3)*exp(q*Eg0/n/k*(-1/T_p+1/Tr));
     Vt      = k*T_p/q;
+    Ish     = ( V * Np/Ns + I_p*Rs) / Rsh
 
-    I_p   = Np*Iph-Np*I0*(exp((V/Ns+I_p*Rs/Np)/n/Vt)-1)-(V*Np/Ns+I_p*Rs)/Rsh;
+    I_p   = Np*Iph - Np*I0*(exp((V/Ns+I_p*Rs/Np)/n*Vt)-1) - Ish;
 end
